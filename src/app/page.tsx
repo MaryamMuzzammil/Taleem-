da0a6ee06337161836e0a824a7b5a1d0508cba7f
MaryamMuzzammil <tumharaemail@example.com>
2025-06-24 11:49:58 +0500

    changes in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import Navbar from "@/components/navbar";
-import Image from "next/image";
-import ChatPage from "./chat/page";
 
 export default function Home() {
   return (
    <>
-   <Navbar/>
-   <ChatPage/>
+
    </>
   );
 }
